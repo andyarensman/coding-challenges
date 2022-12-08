7708c50b5e2d174be88bff81e2ba08b3acc39d29
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-12-07 18:51:29 -0800

    rules

---
+++ freeCodeCamp/Algorithms/BinarySearch.js
@@ -0,0 +1,10 @@
+/*
+Implement Binary Search
+
+Binary search is an O(log(n)) efficiency algorithm for searching a sorted array to find an element. It operates using the following steps:
+
+Find the middle value of a sorted array. If value == target return (found it!).
+If middle value < target, search right half of array in next compare.
+If middle value > target, search left half of array in next compare.
+As you can see, you are successively halving an array, which gives you the log(n) efficiency. For this challenge, we want you to show your work - how you got to the target value... the path you took!
+*/
