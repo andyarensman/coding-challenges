66710821ccd67f82194ab6b95eda1894c55092b2
andyarensman <86622025+andyarensman@users.noreply.github.com>
2022-12-14 12:19:35 -0800

    set class

---
+++ freeCodeCamp/DataStructures/SetClass.js
@@ -0,0 +1,43 @@
+class Set {
+  constructor() {
+    // Dictionary will hold the items of our set
+    this.dictionary = {};
+    this.length = 0;
+  }
+
+  // This method will check for the presence of an element and return true or false
+  has(element) {
+    return this.dictionary[element] !== undefined;
+  }
+
+  // This method will return all the values in the set
+  values() {
+    return Object.values(this.dictionary);
+  }
+
+  // Only change code below this line
+  add(item) {
+    if (!this.has(item)) {
+      this.dictionary[item] = item
+      this.length++
+      return true
+    } else {
+      return false
+    }
+  }
+  
+  remove(item) {
+    if (!this.has(item)) {
+      return false
+    } else {
+      delete this.dictionary[item]
+      this.length--
+      return true
+    }
+  }
+
+  size() {
+    return this.length
+  }
+  // Only change code above this line
+}
